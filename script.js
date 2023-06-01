a79541b8de0ebe4b8a0e51414fb9b822d9c1649d
kathy yost <kathy.yost@student.centralia.edu>
2023-06-01 09:23:16 -0700

    added stuff

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const data=
+{
+
+brandName: "Smash Games",
+pages:[
+    {
+      name: "index",
+      PageName: "Home",
+      callToAction:{
+        src: "images/SmashGamesLogo-short.png",
+        altText: "Smash Games Logo"
+      }
+    },
+    {
+        name: "blast",
+        PageName: "Inferno Blast",
+        callToAction:{
+            src: "images/Inferno-Jumborton.png",
+            altText: "Inferno Blast Gameplay",
+            buttonlinkSrc: "https://steampowered.com",
+            buttonlinkText: "Buy now on Steam"
+        }
+    }
+]   
+}
